119634a014d44d51f35972137d23ab7efbe54f74
Duranskinho <duranski17@gmail.com>
2017-03-22 20:18:19 +1300

    created shader class

---
+++ Mengo3D/src/Model.h
@@ -0,0 +1,38 @@
+#ifndef _MODEL_H
+#define _MODEL_H
+
+#include <d3d11.h>
+#include <DirectXMath.h>
+using namespace DirectX;
+
+class Model
+{
+public:
+
+	struct VertexType
+	{
+		XMFLOAT3 position;
+		XMFLOAT4 color;
+	};
+
+public:
+	Model();
+	~Model();
+
+	bool Initialize(ID3D11Device* device);
+	void Shutdown();
+	void Render(ID3D11DeviceContext* deviceContext);
+
+	int GetIndexCount();
+
+private:
+	bool InitializeBuffers(ID3D11Device* device);
+	void ShutdownBuffers();
+	void RenderBuffers(ID3D11DeviceContext* deviceContext);
+	
+	ID3D11Buffer* m_vertexBuffer, *m_indexBuffer;
+	int m_vertexCount, m_indexCount;
+
+};
+
+#endif
