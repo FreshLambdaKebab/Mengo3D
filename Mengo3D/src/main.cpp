b8293e3d55ca0e8c8a66c750af45b53e7a80ced0
Duranskinho <duranski17@gmail.com>
2017-10-06 11:28:52 +1300

    fresh start
    Rewriting entire project from scratch

--- Mengo3D/src/main.cpp
+++ Mengo3D/src/main.cpp
@@ -1,28 +1,9 @@
-#include "Framework.h"
+#include <Windows.h>
+#include <iostream>
 
-int WINAPI WinMain(HINSTANCE hInst,HINSTANCE hPrevInst,LPSTR lpCmdLine,int nShowCmd)
+int WINAPI WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance,PSTR pScmdLine,int iCmdShow)
 {
-	Framework* framework;
-	bool result;
-	
-	//create the framework object pointer
-	framework = new Framework();
-	if (!framework)
-	{
-		return 0;
-	}
-
-	//initialize and run the framework object
-	result = framework->Init();
-	if (result)
-	{
-		framework->Run();
-	}
-	
-	//shut down and get rid of the framework object
-	framework->Shutdown();
-	delete framework;
-	framework = 0;
+		
 
 	return 0;
 }
